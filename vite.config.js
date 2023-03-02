da5b7539ff9cd01d9bf9f0791fb4195ec2438658
John F Morton <morton@jmx2.com>
2023-03-02 13:10:45 -0500

    build files into demo directory

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,15 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: 'https://johnfmorton.github.io/vite-module-w-ghpages-demo-explore/', /// Set this to './' if your site is deployed at https://<USERNAME>.github.io/<REPO>/.
-  // build: {
-  //   lib: {
-  //     entry: path.resolve(__dirname, 'lib/main.ts'),
-  //     name: 'progressive-share-button',
-  //     fileName: (format) => `progressive-share-button.${format}.js`
-  //   },
-  //   minify: false
-  // }
+  build: {
+    outDir: 'demo',
+    // lib: {
+    //   entry: path.resolve(__dirname, 'lib/main.ts'),
+    //   name: 'progressive-share-button',
+    //   fileName: (format) => `progressive-share-button.${format}.js`
+    // },
+    minify: false
+  }
 })
 
 // module.exports = defineConfig({
