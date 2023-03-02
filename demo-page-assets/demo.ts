6fc3775710de0b01a93ef936103f0a7a995002ac
John F Morton <morton@jmx2.com>
2023-03-02 16:21:26 -0500

    moved assets to demo-page-assets directory

--- demo-page-assets/demo.ts
+++ demo-page-assets/demo.ts
@@ -1,2 +1,10 @@
-import ModuleDemoPage from '../lib/main';
-ModuleDemoPage.init("Success! The module is working.")
+// Path: demo-page-assets/demo.ts
+// This is the entry point for the demo page. It's a TypeScript file that
+//  loads in the module that we're buidling with this repo
+import ModuleDemoPage from '../lib/main'
+ModuleDemoPage.init('Success! The module is working.')
+
+import './style.css';
+
+// From here, you can add any additional JavaScript you want to run on the demo page.
+// For example, you could add a button that calls a function in the module.
