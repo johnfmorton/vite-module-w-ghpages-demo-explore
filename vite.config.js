72204cdf04fdeef2d39e0701fcd810da552cfa55
John F Morton <morton@jmx2.com>
2023-03-03 16:23:08 -0500

    updated referenced filename

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
 // This is the config file used to compile the module that will be published to NPM.
 const path = require('path')
 const { defineConfig } = require('vite')
+import banner from 'vite-plugin-banner'
+import pkg from './package.json'
+
+// Now in UTC time. Format time as YYYY-MM-DDTHH:mm:ss.sssZ.
+const now = new Date().toISOString()
 
 module.exports = defineConfig({
     build: {
         lib: {
             entry: path.resolve(
                 __dirname,
-                'lib/vite-module-w-ghpages-demo-explore.ts'
+                'lib/vite-module-builder-w-ghpages-npm-template.ts'
             ),
             name: 'vite-module-builder-w-ghpages-npm-template',
             fileName: (format) =>
@@ -15,4 +20,9 @@ module.exports = defineConfig({
         },
         minify: false,
     },
+    plugins: [
+        banner(
+            `/**\n * name: ${pkg.name}\n * version: v${pkg.version}\n * description: ${pkg.description}\n * author: ${pkg.author}\n * repository: ${pkg.repository.url}\n * build date: ${now} \n */`
+        ),
+    ],
 })
